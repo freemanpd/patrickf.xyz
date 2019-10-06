b0769f2bab474da9799ad5fa3f9c122929d8ccfe
freemanpd <patrick.freeman@sftech.io>
2019-10-06 01:34:17 -0400

    testing docker build updates

--- patrickf-backend/settings.py
+++ patrickf-backend/settings.py
@@ -24,8 +24,9 @@ FRONTEND_DIR = os.path.abspath(
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DBSECRETKEY']
-DEBUG = os.environ['DEBUGLOG']
+SECRET_KEY = os.environ('DBSECRETKEY', 'password123password123')
+DEBUG = os.environ('DEBUGLOG', 'FALSE')
+
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -100,13 +101,15 @@ WSGI_APPLICATION = 'patrickf-backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DBNAME'],
-        'HOST': os.environ['DBHOST'],
-        'USER': os.environ['DBUSER'],
-        'PASSWORD': os.environ['DBPASS'],
-        'PORT': os.environ['DBPORT'],
+        'NAME': os.environ('DBNAME', 'devdb'),
+        'HOST': os.environ('DBHOST', '127.0.0.1'),
+        'USER': os.environ('DBUSER', 'postgres'),
+        'PASSWORD': os.environ('DBPASS', 'password123'),
+        'PORT': os.environ('DBPORT', '5432'),
     }
 }
+    
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
